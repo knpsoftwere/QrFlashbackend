1c47f413784cb1fc3442cadbe3654204478ba09c
hikinokouji <steamon1me@gmail.com>
2024-11-12 00:22:01 +0200

    init back

---
+++ src/main/java/org/qrflash/qrflashbackend/QrFlashbackendApplication.java
@@ -0,0 +1,13 @@
+package org.qrflash.qrflashbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrFlashbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrFlashbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/qrflash/qrflashbackend/QrFlashbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.qrflash.qrflashbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrFlashbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
