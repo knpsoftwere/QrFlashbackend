db4f535cfa5980c0f2ac89fe1c4a676ee6b6a612
hikinokouji <steamon1me@gmail.com>
2024-11-12 01:45:43 +0200

    add application.properties file

---
+++ src/main/java/org/qrflash/qrflashbackend/Controller/HealthController.java
@@ -0,0 +1,12 @@
+package org.qrflash.qrflashbackend.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "Application is running!";
+    }
+}
