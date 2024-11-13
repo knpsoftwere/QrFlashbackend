8ef8ecda2c6176dd6ce50ef67716810cd6f041f9
hikinokouji <steamon1me@gmail.com>
2024-11-14 00:54:02 +0200

    add application.properties file

--- src/main/java/org/qrflash/Controller/UpdateController.java
+++ src/main/java/org/qrflash/Controller/UpdateController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class UpdateController {
     @GetMapping("/contrl")
     public String updateCheck() {
-        return "lasteUpdate";
+        return "mega last updatwe";
     }
 }
