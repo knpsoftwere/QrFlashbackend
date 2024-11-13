3f62b97b7dc2491fac782a6f6fa2c027b9584028
hikinokouji <steamon1me@gmail.com>
2024-11-13 23:53:09 +0200

    add application.properties file

--- src/main/java/org/qrflash/Controller/UpdateController.java
+++ src/main/java/org/qrflash/Controller/UpdateController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class UpdateController {
     @GetMapping("/contrl")
     public String updateCheck() {
-        return "loadsasdsadasdawsfawewaf";
+        return "boshe upasi";
     }
 }
