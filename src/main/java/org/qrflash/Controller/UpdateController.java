3c264aac3d0be54dee5283f33768070a43fdde79
hikinokouji <steamon1me@gmail.com>
2024-11-13 21:07:43 +0200

    add application.properties file

--- src/main/java/org/qrflash/Controller/UpdateController.java
+++ src/main/java/org/qrflash/Controller/UpdateController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class UpdateController {
     @GetMapping("/contrl")
     public String updateCheck() {
-        return "Appdate application fun git request!";
+        return "lololo!";
     }
 }
