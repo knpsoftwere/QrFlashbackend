1765263bfdaa6041e2ad42297d88a47375c36c49
hikinokouji <steamon1me@gmail.com>
2024-11-17 20:36:39 +0200

    Application register/loguot

--- src/main/java/org/qrflash/Config/WebConfig.java
+++ src/main/java/org/qrflash/Config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://138.201.118.129") // замініть на актуальний домен фронтенду
+                .allowedOrigins("http://qrflash.online") // замініть на актуальний домен фронтенду
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
