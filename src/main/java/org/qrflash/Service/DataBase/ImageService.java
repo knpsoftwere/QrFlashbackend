578aa8c4bf30a62417759713d11cc5277b7308d4
develop <steamon1me@gmail.com>
2026-01-03 22:10:03 +0100

    New fix-IP #1

--- src/main/java/org/qrflash/Service/DataBase/ImageService.java
+++ src/main/java/org/qrflash/Service/DataBase/ImageService.java
@@ -96,7 +96,7 @@ public class ImageService {
                             .build()
             );
 
-            return url.replace("http://minio:9000", "http://51.21.255.211/qrflash-images");
+            return url.replace("http://minio:9000", "http://13.62.51.144/qrflash-images");
         }catch(Exception e){
             throw  new RuntimeException("Error creating bucket", e);
         }
